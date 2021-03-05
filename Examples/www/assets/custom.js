9d0aead5150eb0fa9d1639f82693353576a185d5
Illia Derevianko <illia.derevianko@xfel.eu>
2021-03-05 15:23:17 +0100

    HTML gauges

--- Examples/www/assets/custom.js
+++ Examples/www/assets/custom.js
@@ -36,7 +36,66 @@ function initTable(table, file){
     csvToTable(table, file);
 }
 
+let gauges = {};
+
+function refreshGauges(){
+    $.get("example3.csv", function(response) {
+        let data = $.csv.toObjects(response)[0];
+        gauges.air_p.set(data.air_p);
+    });
+}
+
+function InitGauges(){
+    //air preasure
+    let opts = {
+      angle: -0.25,
+      lineWidth: 0.07,
+      radiusScale: 1,
+      pointer: {
+        length: 0.46,
+        strokeWidth: 0.013,
+        color: '#000000'
+      },
+      limitMax: false,
+      limitMin: false,
+      colorStart: '#6FADCF',
+      colorStop: '#8FC0DA',
+      strokeColor: '#E0E0E0',
+      generateGradient: true,
+      highDpiSupport: true,
+        staticZones: [
+           {strokeStyle: "#CCCCCC", min: 960, max: 1010},
+           {strokeStyle: "#FFDD00", min: 1010, max: 1060},
+        ],
+      staticLabels: {font: "10px",  labels: [970, 990, 1010, 1030, 1050]},
+      renderTicks: {
+          divisions: 10,
+          divColor: '#333333',
+          subDivisions: 10,
+          subLength: 0.5,
+          subWidth: 0.3,
+          subColor: '#666666',
+        }
+    };
+    let gauge = new Gauge(document.getElementById('g_air_p')).setOptions(opts);
+    gauge.setTextField(document.getElementById("g_air_p_text"));
+    gauge.maxValue = 1060;
+    gauge.setMinValue(960);
+    gauge.animationSpeed = 32;
+    gauge.set(960);
+    gauges.air_p = gauge;
+
+
+}
+
+function _refreshGauges(data){
+    gPreasure(data.air_p);
+    console.log(data);
+}
+
 $(document).ready(function() {
     initTable($('#log'), "example1.csv");
     initTable($('#debug'), "example2.csv");
+    InitGauges();
+    setInterval(refreshGauges, 5000); //refresh each 5 sec
 } );
